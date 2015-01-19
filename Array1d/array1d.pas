9746acf4ac92934cd1e5dc112da79192d9190eb2
vakovsky <vakovsky@gmail.com>
2015-01-19 02:55:02 +0200

    2

---
+++ Array1d/array1d.pas
@@ -0,0 +1,77 @@
+program Lab_;
+
+const
+  n = 3;
+
+type
+  array1d = array[1..n] of integer;
+
+var
+  a: array1d;
+
+  procedure array1d_read(var a: array1d);
+  var
+    i: integer;
+  begin
+    for i := 1 to n do
+    begin
+      Write('a[', i, ']=');
+      readln(a[i]);
+    end;
+  end;
+
+  procedure array1d_writeList(a: array1d);
+  var
+    i: integer;
+  begin
+    for i := 1 to n do
+    begin
+      writeln('a[', i, ']=', a[i]);
+    end;
+  end;
+
+  procedure array1d_writeInARow(a: array1d);
+  var
+    i: integer;
+  begin
+    for i := 1 to n do
+    begin
+      Write(a[i], ', ');
+    end;
+    writeln;
+  end;
+
+  procedure array1d_writeZeros(a: array1d);
+  var
+    i: integer;
+  begin
+    for i := 1 to n do
+    begin
+      if a[i] = 0 then
+      begin
+        writeln('a[', i, ']=', a[i]);
+      end;
+    end;
+  end;
+
+begin
+  writeln;
+  writeln('Read Array');
+  array1d_read(a);
+
+  writeln;
+  writeln('Write Array List');
+  array1d_writeList(a);
+
+  writeln;
+  writeln('Write Array In a row');
+  array1d_writeInARow(a);
+
+  writeln;
+  writeln('Write Array Zeros');
+  array1d_writeZeros(a);
+
+  writeln;
+  Write('Press <Enter>');
+  readln;
+end.
